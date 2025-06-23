c021b887c2982dc4985b5e6ec95be3ac3930dfeb
Cho-Hyun-Seung <toki0327@naver.com>
2025-06-23 17:31:08 +0900

    feat: 인플루언서 유튜브 연동

---
+++ src/features/campaign/constants/filterOptions.js
@@ -0,0 +1,12 @@
+export const categoryOptions = [
+    { value: 'title', label: '제목' },
+    { value: 'clientCompany', label: '고객사' },
+    { value: 'user', label: '담당자' },
+];
+
+export const filterOptions = [
+    { value: 1, label: '승인완료' },
+    { value: 2, label: '승인요청' },
+    { value: 3, label: '보류/대기' },
+    { value: 4, label: '거절됨' },
+];
--- src/features/user/api.js
+++ src/features/user/api.js
@@ -1,6 +1,15 @@
-import axios from 'axios'
+import axios from 'axios';
+import api from '@/plugin/axios.js';
 
 export const getMockInfluencers = async () => {
-  const response = await axios.get('/api/v1/influencer')
-  return response
-}
+    const response = await axios.get('/api/v1/influencer');
+    return response;
+};
+
+export const connectYoutube = async () => {
+    return await api.get('/oauth2/authorize/youtube');
+};
+
+export const postToken = async (code) => {
+    return await api.post('/oauth2/youtube/code', { code: code });
+};
