a5c758b384bbcd725a048598cc730f521a091fc7
Cho-Hyun-Seung <toki0327@naver.com>
2025-07-03 14:47:49 +0900

    chore: 로그인 연동

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,54 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { mainRoutes } from '@/features/main/router.js'
-import { advertisementRoutes } from '@/features/advertisement/router'
-import { calendarRoutes } from '@/features/calendar/router'
-import { campaignRoutes } from '@/features/campaign/router'
-import { chatRoutes } from '@/features/chat/router'
-import { contractRoutes } from '@/features/contract/router'
-import { userRoutes } from '@/features/user/router'
+import { createRouter, createWebHistory } from 'vue-router';
+import { mainRoutes } from '@/features/main/router.js';
+import { advertisementRoutes } from '@/features/advertisement/router';
+import { calendarRoutes } from '@/features/calendar/router';
+import { campaignRoutes } from '@/features/campaign/router';
+import { chatRoutes } from '@/features/chat/router';
+import { contractRoutes } from '@/features/contract/router';
+import { userRoutes } from '@/features/user/router';
 import { influencerRoutes } from '@/features/influencer/router.js';
 import { myPageRoutes } from '@/features/mypage/router.js';
-import {aiRoutes} from "@/features/ai/router.js";
+import { aiRoutes } from '@/features/ai/router.js';
 import { dashboardRoutes } from '@/features/dashboard/router.js';
+import { useAuthStore } from '@/stores/auth';
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    ...mainRoutes,
-    ...advertisementRoutes,
-    ...calendarRoutes,
-    ...campaignRoutes,
-    ...chatRoutes,
-    ...contractRoutes,
-    ...userRoutes,
-    ...influencerRoutes,
-    ...myPageRoutes,
-    ...aiRoutes,
-    ...dashboardRoutes,
-  ],
-  scrollBehavior() {
-    return { left: 0, top: 0 }
-  },
-})
+    history: createWebHistory(),
+    routes: [
+        ...mainRoutes,
+        ...advertisementRoutes,
+        ...calendarRoutes,
+        ...campaignRoutes,
+        ...chatRoutes,
+        ...contractRoutes,
+        ...userRoutes,
+        ...influencerRoutes,
+        ...myPageRoutes,
+        ...aiRoutes,
+        ...dashboardRoutes,
+    ],
+    scrollBehavior() {
+        return { left: 0, top: 0 };
+    },
+});
 
-export default router
+router.beforeEach((to) => {
+    const authStore = useAuthStore();
+    const nonAuthRequirePages = ['login', 'findPassword', 'changePassword'];
+
+    const isNonAuthPage = nonAuthRequirePages.includes(to.name);
+    console.log('isNoAuthPage : ', isNonAuthPage);
+    console.log('isAuthenticated : ', authStore.isAuthenticated);
+
+    // 인증이 필요한 페이지인데 인증되지 않은 경우
+    if (!isNonAuthPage && !authStore.isAuthenticated) {
+        return { name: 'login' };
+    }
+
+    // 인증된 사용자가 로그인, 회원가입 등의 비인증 페이지로 이동 시도
+    if (isNonAuthPage && authStore.isAuthenticated) {
+        return { name: 'main' };
+    }
+});
+
+export default router;
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,1 +1,47 @@
-/* auth.js */
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+export const useAuthStore = defineStore('auth', () => {
+    const accessToken = ref(null);
+    const userId = ref(null);
+    const expirationTime = ref(null);
+
+    const isAuthenticated = computed(() => {
+        console.log('엑세스토큰 : ', accessToken.value);
+        return !!accessToken.value && Date.now() < (expirationTime.value || 0);
+    });
+
+    function setAccessToken(token) {
+        accessToken.value = token;
+        try {
+            const payload = JSON.parse(atob(token.split('.')[1]));
+            userId.value = payload.userId;
+            expirationTime.value = payload.exp * 1000;
+        } catch (e) {
+            console.log('엑세스 토큰 파싱 에러', e);
+            accessToken.value = null;
+            userId.value = null;
+            expirationTime.value = null;
+        }
+    }
+
+    function clearAccessToken() {
+        accessToken.value = null;
+        userId.value = null;
+        expirationTime.value = null;
+    }
+
+    function logout() {
+        clearAccessToken();
+        router.push({ name: 'login' });
+    }
+
+    return {
+        userId,
+        expirationTime,
+        accessToken,
+        isAuthenticated,
+        setAccessToken,
+        clearAccessToken,
+        logout,
+    };
+});
