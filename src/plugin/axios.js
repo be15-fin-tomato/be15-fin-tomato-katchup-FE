5d0a8c7642e652f5a96555c4a70d41ee14f14ddf
Cho-Hyun-Seung <toki0327@naver.com>
2025-07-08 14:21:46 +0900

    chore: https 배포 테스트

--- src/plugin/axios.js
+++ src/plugin/axios.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: import.meta.env.VITE_API_BASE_URL,
+    // 임시 배포용 URL => 매번 바뀜
     baseURL: 'https://3.35.216.224:8080/api/v1',
     withCredentials: true,
 });
