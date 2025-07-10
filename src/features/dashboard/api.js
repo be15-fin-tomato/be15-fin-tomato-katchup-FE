216988a57520a24f6e911a09c3f803650bf95e58
lddocy <jkcy911219@naver.com>
2025-07-10 18:21:18 +0900

    feat: 유튜브 인기 동영상 리스트, 진행 완료 캠페인 조회 api 작성

--- src/features/dashboard/api.js
+++ src/features/dashboard/api.js
@@ -25,5 +25,15 @@ export const fetchSatisfaction = async (influencerId) => {
   return await api.get(`/satisfaction/average/${influencerId}`);
 }
 
+// 인플루언서 인기 동영상 조회
+export const fetchTopVideos = async (influencerId) => {
+  return await api.get(`/oauth2/youtube/top-videos`, {
+    params: { influencerId },
+  });
+}
 
+// 인플루언서 진행 완료 캠페인 조회
+export const fetchCampaignListByInfluencer = async (influencerId) => {
+  return await api.get(`/dashboard/campaign/${influencerId}`);
+}
 
