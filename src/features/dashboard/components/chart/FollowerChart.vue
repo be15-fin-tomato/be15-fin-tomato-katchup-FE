b63f03c5e1e94f47e0460227fc451516e84470ed
lddocy <jkcy911219@naver.com>
2025-07-13 12:04:41 +0900

    feat: 인스타그램 팔로워 차트 api 연동

--- src/features/dashboard/components/chart/FollowerChart.vue
+++ src/features/dashboard/components/chart/FollowerChart.vue
@@ -3,32 +3,65 @@
 import { computed } from 'vue';
 
 const props = defineProps({
-  platform: String,
-  data: Object,
+  platform: {
+    type: String,
+    required: true
+  },
+  data: {
+    type: Object,
+    default: () => ({})
+  },
 });
 
-const dailyChange = computed(() => props.data?.subscriberChangeDaily || 0);
-const weeklyChange = computed(() => props.data?.subscriberChangeWeekly || 0);
-const monthlyChange = computed(() => props.data?.subscriberChangeMonthly || 0);
+const dailyChange = computed(() => {
+  if (props.platform === 'youtube') {
+    return props.data?.subscriberChangeDaily || 0;
+  } else if (props.platform === 'instagram') {
+    return props.data?.followerChangeDaily || 0;
+  }
+  return 0;
+});
+
+const weeklyChange = computed(() => {
+  if (props.platform === 'youtube') {
+    return props.data?.subscriberChangeWeekly || 0;
+  } else if (props.platform === 'instagram') {
+    return props.data?.followerChangeWeekly || 0;
+  }
+  return 0;
+});
+
+const monthlyChange = computed(() => {
+  if (props.platform === 'youtube') {
+    return props.data?.subscriberChangeMonthly || 0;
+  } else if (props.platform === 'instagram') {
+    return props.data?.followerChangeMonthly || 0;
+  }
+  return 0;
+});
+
+const chartTitle = computed(() => {
+  return props.platform === 'youtube' ? '구독자 증감수' : '팔로워 증감수';
+});
 </script>
 
 <template>
   <div class="dashboard-section">
     <p class="dashboard-title">
-      구독자 증감수
+      {{ chartTitle }}
     </p>
     <div class="flex flex-wrap justify-around p-5">
       <div class="flex flex-col items-center justify-center border border-gray-dark rounded-xl shadow-lg">
         <span class="bg-btn-yellow/90 px-30 py-3 text-xl rounded-t-xl">일간</span>
-        <span class="p-15 text-3xl">{{ dailyChange > 0 ? '+' : '' }}{{ dailyChange }}</span>
+        <span class="p-15 text-3xl">{{ dailyChange > 0 ? '+' : '' }}{{ dailyChange.toLocaleString() }}</span>
       </div>
       <div class="flex flex-col items-center justify-center border border-gray-dark rounded-xl shadow-lg">
         <span class="bg-btn-yellow/90 px-30 py-3 text-xl rounded-t-xl">주간</span>
-        <span class="p-15 text-3xl">{{ weeklyChange > 0 ? '+' : '' }}{{ weeklyChange }}</span>
+        <span class="p-15 text-3xl">{{ weeklyChange > 0 ? '+' : '' }}{{ weeklyChange.toLocaleString() }}</span>
       </div>
       <div class="flex flex-col items-center justify-center border border-gray-dark rounded-xl shadow-lg">
         <span class="bg-btn-yellow/90 px-30 py-3 text-xl rounded-t-xl">월간</span>
-        <span class="p-15 text-3xl">{{ monthlyChange > 0 ? '+' : '' }}{{ monthlyChange }}</span>
+        <span class="p-15 text-3xl">{{ monthlyChange > 0 ? '+' : '' }}{{ monthlyChange.toLocaleString() }}</span>
       </div>
     </div>
   </div>
