4b6611b86a0f325440772e69b881e662f9826c7a
lddocy <jkcy911219@naver.com>
2025-07-13 10:44:41 +0900

    feat: 인스타그램 계정 정보 조회 (인기 게시물 포함) api 등록

--- src/features/dashboard/api.js
+++ src/features/dashboard/api.js
@@ -13,6 +13,11 @@ export const fetchInfluencerDetail = async (influencerId) => {
   return res.data.data
 }
 
+// 인플루언서 평균 만족도 조회
+export const fetchSatisfaction = async (influencerId) => {
+  return await api.get(`/satisfaction/average/${influencerId}`);
+}
+
 // 인플루언서 유튜브 기본 정보 조회
 export const fetchYoutubeInfo = async (influencerId) => {
   return await api.get('/oauth2/youtube/channel-info', {
@@ -20,12 +25,7 @@ export const fetchYoutubeInfo = async (influencerId) => {
   });
 }
 
-// 인플루언서 평균 만족도 조회
-export const fetchSatisfaction = async (influencerId) => {
-  return await api.get(`/satisfaction/average/${influencerId}`);
-}
-
-// 인플루언서 인기 동영상 조회
+// 인플루언서 유튜브 인기 동영상 조회
 export const fetchTopVideos = async (influencerId) => {
   return await api.get(`/oauth2/youtube/top-videos`, {
     params: { influencerId },
@@ -37,3 +37,9 @@ export const fetchCampaignListByInfluencer = async (influencerId) => {
   return await api.get(`/dashboard/campaign/${influencerId}`);
 }
 
+// 인스타그램 계정 정보 조회 (인기 게시물 포함)
+export const fetchInstagramInfo = async (influencerId) => {
+  return await api.get(`/oauth2/instagram/account-info`, {
+    params: { influencerId },
+  });
+}
