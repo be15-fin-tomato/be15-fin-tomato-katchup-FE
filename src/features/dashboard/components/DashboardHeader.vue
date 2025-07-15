f292de8779a268f7482d35f1f731dae5c380d03c
lddocy <jkcy911219@naver.com>
2025-07-15 20:57:44 +0900

    fix: 대시보드 헤더 null 값 방어

--- src/features/dashboard/components/DashboardHeader.vue
+++ src/features/dashboard/components/DashboardHeader.vue
@@ -52,19 +52,19 @@ const formatGender = (gender) => {
 
       <!-- 썸네일 -->
       <img
-        :src="influencer.youtube.thumbnailUrl"
+        :src="influencer?.youtube?.thumbnailUrl"
         alt="썸네일"
         class="w-32 h-32 rounded-full object-cover"
       />
 
       <!-- 유튜브 채널명 -->
       <div class="flex-1">
-        <h1 class="text-2xl font-bold">{{ influencer.youtube.name }}</h1>
+        <h1 class="text-2xl font-bold">{{ influencer?.youtube?.name }}</h1>
 
         <!-- 태그 -->
         <div class="flex flex-wrap gap-2 mt-2 mb-5">
           <span
-            v-for="tag in influencer.tags ?? []"
+            v-for="tag in influencer?.tags ?? []"
             :key="tag.categoryId"
             class="text-sm rounded-lg px-2"
             :class="tagStyle(tag.categoryName)"
@@ -76,11 +76,11 @@ const formatGender = (gender) => {
           <div class="flex flex-col gap-2">
             <div class="flex items-center gap-3 text-md text-gray-dark">
               <Icon icon="logos:youtube-icon" class="w-7 h-7" />
-              <span>구독자 {{ formatCount(influencer?.youtube.subscriber) }}</span>
+              <span>구독자 {{ formatCount(influencer?.youtube?.subscriber) }}</span>
             </div>
             <div class="flex items-center gap-3 text-md text-gray-dark">
               <Icon icon="skill-icons:instagram" class="w-7 h-7" />
-              <span>팔로워 {{ formatCount(influencer?.instagram.follower) }}</span>
+              <span>팔로워 {{ formatCount(influencer?.instagram?.follower) }}</span>
             </div>
           </div>
       </div>
@@ -89,18 +89,18 @@ const formatGender = (gender) => {
           <div class="flex items-center justify-center gap-3">
             <span><Icon icon="mingcute:target-line" width="26" height="26" /></span>
             <span>타깃 : </span>
-            <span class="bg-pipeline-proposal/50 px-2 rounded-md">{{ influencer.ageRange ?? '미정' }}</span>
-            <span :class="[genderColor(influencer.targetGender), 'px-2 rounded-md']">{{ formatGender(influencer.targetGender) }}</span>
+            <span class="bg-pipeline-proposal/50 px-2 rounded-md">{{ influencer?.ageRange ?? '미정' }}</span>
+            <span :class="[genderColor(influencer?.targetGender), 'px-2 rounded-md']">{{ formatGender(influencer?.targetGender) }}</span>
           </div>
           <div class="flex items-center justify-center gap-3">
             <span><Icon icon="fluent-emoji-high-contrast:money-bag" width="26" height="26" /></span>
             <span>단가 : </span>
-            <span>{{ influencer.price?.toLocaleString() ?? '미정' }}원</span>
+            <span>{{ influencer?.price?.toLocaleString() ?? '미정' }}원</span>
           </div>
           <div class="flex items-center justify-center gap-3">
             <span><Icon icon="hugeicons:manager" width="25" height="25" /></span>
             <span>매니저 : </span>
-            <span>{{ influencer.manager ?? '없음' }}</span>
+            <span>{{ influencer?.manager ?? '없음' }}</span>
           </div>
         </div>
     </div>
