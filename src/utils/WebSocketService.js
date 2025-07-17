575dddaf05c56449a6f5f9e4b19ce54625ef8cfc
Joonspar <sdwang15@naver.com>
2025-07-17 11:43:17 +0900

    채팅 연결

--- src/utils/WebSocketService.js
+++ src/utils/WebSocketService.js
@@ -5,7 +5,7 @@ import { Client } from '@stomp/stompjs'
 let stompClient = null
 
 export const connectWebSocket = (roomId, onMessage, token) => {
-  const socket = new SockJS('https://api.tomato-katchup.xyz/api/v1/ws')
+  const socket = new SockJS('https://api.tomato-katchup.xyz/api/v1/ws-stomp')
   stompClient = new Client({
     webSocketFactory: () => socket,
     connectHeaders: {
