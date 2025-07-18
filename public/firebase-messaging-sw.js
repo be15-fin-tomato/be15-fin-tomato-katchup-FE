dd8c1c652d70ade884602cc207ced837f100bc55
kkkwid <lsj9057@daum.net>
2025-07-18 14:45:23 +0900

    다시변경

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,25 +7,14 @@ firebase.initializeApp({
   messagingSenderId: "101664121020",
   projectId: "tomato-katchup",
   appId: "1:101664121020:web:525beb263a7bbdbc7530b9",
-  authDomain: "tomato-katchup.firebaseapp.com",
+  authDomain:"tomato-katchup.firebaseapp.com",
 });
 
-self.addEventListener('notificationclick', function (event) {
-  event.notification.close();
+const messaging = firebase.messaging();
 
-  // 브라우저가 자동 생성한 알림에는 data.url이 없을 수도 있음
-  const targetUrl = 'https://tomato-katchup.xyz/';
-
-  event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
-      for (const client of windowClients) {
-        if (client.url === targetUrl && 'focus' in client) {
-          return client.focus();
-        }
-      }
-      if (clients.openWindow) {
-        return clients.openWindow(targetUrl);
-      }
-    })
-  );
+messaging.onBackgroundMessage(payload => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: '/tomato.png',
+  });
 });
